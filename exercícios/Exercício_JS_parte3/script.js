d80564e293e26cf654d52e2fe1d75815394716f1
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2024-05-07 01:43:58 -0300

    exercício 4 finalizado (95% sem consulta)

--- exercícios/Exercício_JS_parte3/script.js
+++ exercícios/Exercício_JS_parte3/script.js
@@ -5,13 +5,17 @@ function contar() {
     let res = window.document.getElementById('res')
 
     if (start.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
-        window.alert('[ERRO] Verifique se os dados são maiores que 0')
         res.innerHTML = 'impossível contar!'
+        window.alert('[ERRO] Verifique se os dados são maiores que 0')
     } else {
         res.innerHTML = 'Contando: '
         let s = Number(start.value)
         let e = Number(end.value)
         let p = Number(pass.value)
+        if (p <= 0) {
+            window.alert('Número do passo inválido! Considerando passo 1')
+            p = 1
+        }
         if (s < e) {
             for(let c = s; c <= e; c += p) {
                 res.innerHTML += `${c}➡️`
