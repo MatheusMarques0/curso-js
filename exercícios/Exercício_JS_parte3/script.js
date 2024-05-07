56e8f8ad544cb543879a396b3d37616f74fd3bb8
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2024-05-07 01:35:21 -0300

    exercício parte 4

---
+++ exercícios/Exercício_JS_parte3/script.js
@@ -0,0 +1,26 @@
+function contar() {
+    let start = window.document.getElementById('ini')
+    let end = window.document.getElementById('fim')
+    let pass = window.document.getElementById('pas')
+    let res = window.document.getElementById('res')
+
+    if (start.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
+        window.alert('[ERRO] Verifique se os dados são maiores que 0')
+        res.innerHTML = 'impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let s = Number(start.value)
+        let e = Number(end.value)
+        let p = Number(pass.value)
+        if (s < e) {
+            for(let c = s; c <= e; c += p) {
+                res.innerHTML += `${c}➡️`
+            }
+        } else {
+            for(let c = s; c >= e; c -= p) {
+                res.innerHTML += `${c}➡️`
+            }
+        }
+        res.innerHTML += '🚩'
+        }
+    }
