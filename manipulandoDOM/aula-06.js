301ae2adb6adbddc327088e88d9633a578082132
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-06-19 00:53:08 -0300

    .

---
+++ manipulandoDOM/aula-06.js
@@ -0,0 +1,9 @@
+// querySelector
+
+document.title = "QuerySelector"
+
+document.querySelector("#idqualquer1").style.fontFamily = "Arial"
+
+// querySelectorAll
+
+document.querySelectorAll("#idqualquer")[1].style.fontFamily = "Arial"
