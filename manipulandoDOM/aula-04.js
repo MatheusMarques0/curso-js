eeba5ea189d3265404626703f53d35d171ef12c0
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-06-19 00:05:52 -0300

    .

---
+++ manipulandoDOM/aula-04.js
@@ -0,0 +1,13 @@
+// Aula 04
+
+document.title = "ClassName"
+
+document.body.style.background = "#ccc"
+
+let minhaClasse = document.getElementsByClassName("minhadiv")[0]
+
+minhaClasse.innerHTML = "<p>Olá número <b>3</b></p>"
+
+let minhaClasse2 = document.getElementsByClassName("minhadiv")[2]
+
+minhaClasse2.innerHTML = "Sou o número <em>três</em>"
---
+++ manipulandoDOM/aula-05.js
@@ -0,0 +1,33 @@
+document.title = "TagName"
+
+document.body.style.background = "#fcc"
+
+document.getElementsByTagName("h1")[0].style.backgroundColor = "red" //Fundo vermelho
+
+document.getElementsByTagName("h1")[1].style.backgroundColor = "darkred"
+
+document.getElementsByTagName("h1")[2].style.backgroundColor = "#9D202F" 
+
+document.getElementsByTagName("h1")[2].style.color = "white" //Letra branca
+
+document.getElementsByTagName("div")[0].style.fontSize = "22px" //Tamanho da fonte
+
+document.getElementsByTagName("div")[1].style.fontSize = "22px"
+
+document.getElementsByTagName("div")[2].style.fontSize = "22px"
+
+document.getElementsByTagName("div")[0].style.backgroundColor = "#1E90FF"
+
+document.getElementsByTagName("div")[1].style.backgroundColor = "#0077B6"
+
+document.getElementsByTagName("div")[2].style.backgroundColor = "#000080"
+
+document.getElementsByTagName("div")[2].style.color = "white"
+
+document.getElementsByTagName("p")[0].style.backgroundColor = "#FF00FF"
+
+document.getElementsByTagName("p")[1].style.backgroundColor = "#9A4EAE"
+
+document.getElementsByTagName("p")[2].style.backgroundColor = "#5D3F6A"
+
+document.getElementsByTagName("p")[2].style.color = "white"
