200e2602237cb7e89b226be10688ec4f4430e485
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2025-06-26 21:48:27 -0300

    .

---
+++ manipulandoDOM/aula-08.js
@@ -0,0 +1,11 @@
+document.title = "scrollTo"
+
+document.body.style.background = '#ccc'
+
+function topo() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
